420da5ec3bee005f491e6aa98df403cbbfcde9b0
vijishankar <67778907+vijishankar@users.noreply.github.com>
2021-04-13 13:22:46 +0530

    Update Startup.cs

--- core-web-app/Startup.cs
+++ core-web-app/Startup.cs
@@ -26,7 +26,7 @@ namespace core_web_app
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline .
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
